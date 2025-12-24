9bc65bbca1f9c3143b7d6306e43ddb8be5b42eff
admin <admin@Rijus-MacBookPro-10.local>
2025-12-24 17:06:44 +0530

    Add new UI/UX projects to studio works

--- src/data/studioWorks.ts
+++ src/data/studioWorks.ts
@@ -16,16 +16,32 @@ export const studioWorks: StudioWork[] = [
   {
     id: 'w2',
     title: 'Academy Management',
-    image: '',
-    link: '/studio/academy-management',
-    video: '/Academymanagement.mov'
+    image: '/UIUX/academy%20management/academy.png',
+    link: '/studio/academy-management'
   },
   {
     id: 'w3',
     title: 'Book App',
-    image: '',
-    link: '/studio/book-app',
-    video: '/bookapp.mov'
+    image: '/UIUX/book%20app/bookapp.png',
+    link: '/studio/book-app'
+  },
+  {
+    id: 'w4',
+    title: 'Jobbiz',
+    image: '/UIUX/jobbiz/jobbiz.png',
+    link: '/UIUX/jobbiz/jobbiz.png'
+  },
+  {
+    id: 'w5',
+    title: 'Move On',
+    image: '/UIUX/move%20on/Moveon.png',
+    link: '/UIUX/move%20on/Moveon.png'
+  },
+  {
+    id: 'w6',
+    title: 'Plan My Trip',
+    image: '/UIUX/plan%20my%20trip/planmytrip.png',
+    link: '/UIUX/plan%20my%20trip/planmytrip.png'
   }
 ];
 
